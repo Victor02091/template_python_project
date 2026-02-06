d47ca534378e148ff2bed3637f2d77728ddabbea
Victor Osso <victor.osso.partner@decathlon.com>
2026-02-06 18:29:30 +0100

    fix: use pytest-env to avoid pydantic to crash

--- template/tests/conftest.py
+++ template/tests/conftest.py
@@ -1,11 +1,1 @@
-import pytest
-from pytest import MonkeyPatch
-
-
-@pytest.fixture(autouse=True)
-def mock_env_vars(monkeypatch: MonkeyPatch) -> None:
-    """
-    Mock environment variables for all tests.
-    monkeypatch ensures these are reset after tests finish.
-    """
-    monkeypatch.setenv("ENVIRONMENT", "local")
+# Note: Define default environment variables inside pyroject.toml tool.pytest.ini_options.env
