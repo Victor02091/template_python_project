1e6b55fd5e84656e341d7743425624c05747040d
Victor Osso <victor.osso.partner@decathlon.com>
2026-02-06 14:01:14 +0100

    feat: add logging

--- template/src/{{ project_slug }}/core/config.py
+++ template/src/{{ project_slug }}/core/config.py
@@ -8,7 +8,14 @@ class Settings(BaseSettings):
     """Load environment variables as settings."""
 
     # Define environment variables of the project here
-    ENVIRONMENT: Literal["local", "dev", "preprod", "prod"] = "local"
+
+    # ENVIRONMENT
+    environment: Literal["local", "dev", "preprod", "prod"] = "local"
+
+    # LOG_LEVEL
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = (
+        "INFO"
+    )
 
     # Load dotenv
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
